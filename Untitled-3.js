5b096229e2c558125366ae6110b4f66f609aac1f
LinkinPark1235 <50453828+LinkinPark1235@users.noreply.github.com>
2025-02-20 16:00:48 +0700

    Untitled-3.js

--- Untitled-3.js
+++ Untitled-3.js
@@ -1,5 +1,5 @@
-const startDate = new Date('2025-01-01');
-const endDate = new Date('2025-08-01');
+const startDate = new Date('2025-03-09');
+const endDate = new Date('2025-09-01');
 const currentDate = new Date();
 
 const totalDuration = endDate - startDate;
